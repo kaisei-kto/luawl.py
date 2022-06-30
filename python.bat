539c15cc575c67b61d9ee8e32c9b65315ef3c2d0
kaisei-kto <keshsenpai@gmail.com>
2022-06-30 19:19:50 -0400

    added add_key_tags

---
+++ python.bat
@@ -0,0 +1,6 @@
+@echo off
+rmdir /S /Q dist
+py -m pip install --upgrade build
+py -m build
+py -m twine upload dist/*
+@REM py -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='luawl.py',
     packages=['luawl'],
-    version='1.0.4',
+    version='1.0.5',
     license='MIT',
     description='A python package specifically made for luawl!',
     author='kaisei-kto',
